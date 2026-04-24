f1028d2373276781ba0786ad10197d871f309b79
Viswa-Kalwakady24 <vkalwakady@gmail.com>
2026-04-24 19:35:18 +0530

    Time: 59 ms (86.72%), Space: 19.6 MB (94.76%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -0,0 +1,34 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        if not s or not t:
+            return ""
+        n=len(s)
+        freq={}
+        for ch in t:
+            if ch in freq:
+                freq[ch]+=1
+            else:
+                freq[ch]=1
+        left=0
+        min_len=float('inf')
+        start=0
+        count=0
+        for right in range(n):
+            if s[right]in freq:
+                if freq[s[right]]>0:
+                    count+=1
+                freq[s[right]]-=1
+                while count==len(t):
+                    if (right-left+1)<min_len:
+                        min_len=right-left+1
+                        start=left
+                    if s[left] in freq:
+                        freq[s[left]] += 1
+                        if freq[s[left]] > 0:
+                            count -= 1
+                    left += 1
+        if min_len==float('inf'):
+            return ""
+        else:
+            return s[start:start+min_len]
+        
