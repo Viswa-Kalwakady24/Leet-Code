40cbfab3f45959ce43a2239a27224cd81d734267
Viswa-Kalwakady24 <vkalwakady@gmail.com>
2026-08-21 14:44:31 +0530

    Time: 0 ms (100%), Space: 19.4 MB (13.42%) - LeetHub

--- 2413-smallest-even-multiple/2413-smallest-even-multiple.py
+++ 2413-smallest-even-multiple/2413-smallest-even-multiple.py
@@ -1,5 +1,7 @@
 class Solution:
+
     def smallestEvenMultiple(self, n: int) -> int:
+        """
             smallest=min(2,n)
             largest=max(2,n)
             multiple=0
@@ -8,6 +10,12 @@ class Solution:
                     multiple=largest*i
                     break
             return multiple
+"""
+        if n%2==0:
+            return n
+        else:
+            return n*2
+
 
                 
         
