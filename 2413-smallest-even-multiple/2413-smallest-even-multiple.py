22833a36551513169620c8538ae3c209f3e9fd8d
Viswa-Kalwakady24 <vkalwakady@gmail.com>
2026-08-21 14:42:47 +0530

    Time: 0 ms (100%), Space: 19.4 MB (13.42%) - LeetHub

---
+++ 2413-smallest-even-multiple/2413-smallest-even-multiple.py
@@ -0,0 +1,13 @@
+class Solution:
+    def smallestEvenMultiple(self, n: int) -> int:
+            smallest=min(2,n)
+            largest=max(2,n)
+            multiple=0
+            for i in range(1,smallest+1):
+                if (largest*i)%smallest==0:
+                    multiple=largest*i
+                    break
+            return multiple
+
+                
+        
